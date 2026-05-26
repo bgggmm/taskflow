4a0717564f3a8b0edd69ad57f16dec6bea1e3b44
Brayan <bgm7031@gmail.com>
2026-05-26 14:21:37 -0400

    visual4

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-zinc-50 dark:bg-zinc-950 text-zinc-900 dark:text-zinc-100">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <Navbar />
-          <main className="flex-1 pt-16">{children}</main>
+          <main className="flex-1 pt-0">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
